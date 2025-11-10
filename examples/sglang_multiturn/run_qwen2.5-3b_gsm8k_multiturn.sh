19f300b10b706070aa4b516aa96a4d8e46fcec22
Yu Sun <yusu@nvl72129-T18.cm.cluster>
2025-11-09 21:24:04 -0800

    Modify gsm8k multi-turn script

--- examples/sglang_multiturn/run_qwen2.5-3b_gsm8k_multiturn.sh
+++ examples/sglang_multiturn/run_qwen2.5-3b_gsm8k_multiturn.sh
@@ -8,11 +8,64 @@ ulimit -n 65535
 PROJECT_DIR="$(pwd)"
 CONFIG_PATH="$PROJECT_DIR/examples/sglang_multiturn/config"
 
+
+
+data_dir="/home/yusu/new_home/code/xh/data/gsm8k"
+project_name='xinhao_debug'
+
+ngpus=4
+batch_size=256  # 256
+rollout_name="vllm"  # sglang
+
+# python3 -m verl.trainer.main_ppo \
+#     --config-path="$CONFIG_PATH" \
+#     --config-name='gsm8k_multiturn_grpo' \
+#     algorithm.adv_estimator=grpo \
+#     data.train_batch_size=${batch_size} \
+#     data.max_prompt_length=1024 \
+#     data.max_response_length=1024 \
+#     data.filter_overlong_prompts=True \
+#     data.truncation='error' \
+#     data.return_raw_chat=True \
+#     actor_rollout_ref.model.path=Qwen/Qwen2.5-3B-Instruct \
+#     actor_rollout_ref.actor.optim.lr=1e-6 \
+#     actor_rollout_ref.model.use_remove_padding=True \
+#     actor_rollout_ref.actor.ppo_mini_batch_size=${batch_size} \
+#     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=32 \
+#     actor_rollout_ref.actor.use_kl_loss=True \
+#     actor_rollout_ref.actor.kl_loss_coef=0.001 \
+#     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+#     actor_rollout_ref.actor.entropy_coeff=0 \
+#     actor_rollout_ref.model.enable_gradient_checkpointing=True \
+#     actor_rollout_ref.actor.fsdp_config.param_offload=False \
+#     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+#     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
+#     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+#     actor_rollout_ref.rollout.name=${rollout_name} \
+#     actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
+#     actor_rollout_ref.rollout.n=16 \
+#     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=32 \
+#     actor_rollout_ref.ref.fsdp_config.param_offload=True \
+#     algorithm.use_kl_in_reward=False \
+#     trainer.critic_warmup=0 \
+#     trainer.logger='["console","wandb"]' \
+#     trainer.project_name=${project_name} \
+#     trainer.experiment_name='qwen2.5-3b_function_rm-gsm8k-sgl-multi-w-tool-verify-n16' \
+#     trainer.n_gpus_per_node=${ngpus} \
+#     trainer.nnodes=1 \
+#     trainer.save_freq=-1 \
+#     trainer.test_freq=20 \
+#     data.train_files=${data_dir}/train.parquet \
+#     data.val_files=${data_dir}/test.parquet \
+#     actor_rollout_ref.rollout.multi_turn.tool_config_path="$PROJECT_DIR/examples/sglang_multiturn/config/tool_config/gsm8k_tool_config.yaml" \
+#     trainer.total_epochs=15 \
+#     actor_rollout_ref.rollout.update_weights_bucket_megabytes=512 $@
+
 python3 -m verl.trainer.main_ppo \
     --config-path="$CONFIG_PATH" \
     --config-name='gsm8k_multiturn_grpo' \
     algorithm.adv_estimator=grpo \
-    data.train_batch_size=256 \
+    data.train_batch_size=${batch_size} \
     data.max_prompt_length=1024 \
     data.max_response_length=1024 \
     data.filter_overlong_prompts=True \
@@ -20,11 +73,11 @@ python3 -m verl.trainer.main_ppo \
     data.return_raw_chat=True \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-3B-Instruct \
     actor_rollout_ref.actor.optim.lr=1e-6 \
-    actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=256 \
+    actor_rollout_ref.model.use_remove_padding=False \
+    actor_rollout_ref.actor.ppo_mini_batch_size=${batch_size} \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=32 \
-    actor_rollout_ref.actor.use_kl_loss=True \
-    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.use_kl_loss=False \
+    actor_rollout_ref.actor.kl_loss_coef=0. \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
@@ -32,23 +85,23 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
-    actor_rollout_ref.rollout.name=sglang \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
+    actor_rollout_ref.rollout.name=${rollout_name} \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
     actor_rollout_ref.rollout.n=16 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=32 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.use_kl_in_reward=False \
     trainer.critic_warmup=0 \
     trainer.logger='["console","wandb"]' \
-    trainer.project_name='gsm8k_async_rl' \
+    trainer.project_name=${project_name} \
     trainer.experiment_name='qwen2.5-3b_function_rm-gsm8k-sgl-multi-w-tool-verify-n16' \
-    trainer.n_gpus_per_node=8 \
+    trainer.n_gpus_per_node=${ngpus} \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
     trainer.test_freq=20 \
-    data.train_files=$HOME/data/gsm8k/train.parquet \
-    data.val_files=$HOME/data/gsm8k/test.parquet \
+    data.train_files=${data_dir}/train.parquet \
+    data.val_files=${data_dir}/test.parquet \
     actor_rollout_ref.rollout.multi_turn.tool_config_path="$PROJECT_DIR/examples/sglang_multiturn/config/tool_config/gsm8k_tool_config.yaml" \
     trainer.total_epochs=15 \
+    actor_rollout_ref.rollout.mode="sync" \
     actor_rollout_ref.rollout.update_weights_bucket_megabytes=512 $@
-
